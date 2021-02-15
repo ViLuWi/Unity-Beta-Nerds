3b52fcaf996f3cd6472dee809e9d243174a3ff16
ktxtyo <m.meyerabich@gmail.com>
2021-02-15 17:00:18 +0100

    ...

---
+++ Platform Runner/Assets/Scripts/LevelMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelMenu : MonoBehaviour
+{
+
+    public Animator transition;
+    public float transitionTime = 1f;
+    public string MainGameScene;
+
+    public void PlayLevelOne ()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame ()
+    {
+        SceneManager.LoadScene(MainGameScene);
+    }
+}
