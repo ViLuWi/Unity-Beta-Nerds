ca46ef24a8b2865366a43d9bf953e688cc9e13bd
ViLuWi <write@victor-wilder.de>
2021-02-15 18:35:36 +0400

    no changes

---
+++ Arcade/Assets/Run Boy/Assets/ScoringSystem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScoringSystem : MonoBehaviour
+{
+    public AudioSource collectSound;
+
+
+    void OnTriggerEnter2D(Collider2D collider) {
+        collectSound.Play();
+        LevelMenu.RBStars += 1;
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Arcade/Assets/Run Boy/Assets/Scripts/LevelMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class LevelMenu : MonoBehaviour
+{
+
+    public Animator transition;
+    public float transitionTime = 1f;
+    public string MainGameScene;
+
+    public static int RBStars;
+    public GameObject starDisplay;
+
+    public void Start(){
+        starDisplay.GetComponent<TextMeshProUGUI>().text = "Diamonds: " + RBStars;
+    }
+
+    public void PlayLevelOne ()
+    {
+        SceneManager.LoadScene(1);
+        PauseMenu.GameIsPaused = false;
+    }
+
+    public void QuitGame ()
+    {
+        SceneManager.LoadScene(MainGameScene);
+    }
+}
